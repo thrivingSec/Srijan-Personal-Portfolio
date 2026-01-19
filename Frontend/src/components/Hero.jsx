109e905d771dddef0e24bb03d0a25f3ba2b4c1dd
Srijan <srijanspl2017@gmail.com>
2026-01-19 15:25:16 +0530

    Resume update

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
         <div className="flex items-center justify-center">
           <a
             className="mt-2 mb-2 px-5 py-3 border-2 border-purple-600 hover:border-0 hover:bg-linear-to-r  hover:from-[#1717be] hover:to-[#b910b9] rounded-full text-white text-lg transition-all duration-500 cursor-pointer"
-            href="https://drive.google.com/file/d/153icpl3A71nwgKml8n2nXcqTDTFaUXgr/view?usp=sharing"
+            href="https://drive.google.com/file/d/1A_S_xJuN8scR0ALh5eDZ5rjp8rJYZkSj/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
           >
